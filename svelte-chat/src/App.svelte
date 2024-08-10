ffadd91e34aad5873b983fc673718e51100780bc
Marshall Thompson <marshall@creativeideal.net>
2024-08-09 20:11:03 -0700

    feat: qr code in modal to join a chat room

--- svelte-chat/src/App.svelte
+++ svelte-chat/src/App.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import QRCode from 'qrcode';
   import {
     type AnyDocumentId,
     Repo,
@@ -133,18 +134,54 @@
     });
   };
 
+  let hasQr = false;
+
+  function generateQR() {
+    const canvas = document.createElement('canvas');
+    QRCode.toCanvas(canvas, window.location.href, function (error: any) {
+      if (error) {
+        console.error('error generating qr', error);
+      } else {
+        const base64String = canvas.toDataURL('image/png').split(',')[1];
+        const qr: any = document.getElementById('qr');
+        qr.src = `data:image/png;base64,${base64String}`;
+        hasQr = true;
+      }
+    });
+  }
+
   afterUpdate(() => {
     document
       .getElementById('message-end')
       ?.scrollIntoView({ behavior: 'smooth' });
+    
+    if (!hasQr) {
+      generateQR();
+    }
   });
+
+  let isModalOpen = false;
+  let modalElement: any;
+
+  function openModal() {
+    isModalOpen = true;
+    modalElement?.isModalOpen();
+  }
+
+  function closeModal() {
+    isModalOpen = false;
+    modalElement?.close();
+  }
 
   init();
 </script>
 
 <main>
+  
   {#if ready}
     {#if user === null}
+    
+
       <div
         class="login flex min-h-screen bg-neutral justify-center items-center"
       >
@@ -191,6 +228,14 @@
               <label for="drawer-right" class="text-xs cursor-pointer">
                 <span class="online-count">{users.length}</span> User(s)
               </label>
+            </div>
+            <div class="navbar-end">
+              <!-- Show QR Code button -->
+              <button class="btn btn-circle" on:click={openModal}>
+                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24">
+                  <path fill="currentColor" d="M3 11V3h8v8zm2-2h4V5H5zM3 21v-8h8v8zm2-2h4v-4H5zm8-8V3h8v8zm2-2h4V5h-4zm4 12v-2h2v2zm-6-6v-2h2v2zm2 2v-2h2v2zm-2 2v-2h2v2zm2 2v-2h2v2zm2-2v-2h2v2zm0-4v-2h2v2zm2 2v-2h2v2z"/>
+                </svg>
+              </button>
             </div>
           </div>
           <div id="chat" class="h-full overflow-y-auto px-3">
@@ -258,6 +303,23 @@
           </ul>
         </div>
       </div>
+
+      
+
     {/if}
   {/if}
+
+  <!-- Modal -->
+  <dialog bind:this={modalElement} class="modal w-screen h-screen" class:modal-open={isModalOpen}>
+    <div class="modal-box text-center pt-12">
+      <h3 class="text-xl font-bold">Share DWeb Chat!</h3>
+      <p class="py-4 text-center">Scan or share this QR code to join the chat.</p>
+      <div class="flex items-center justify-center">
+        <img id="qr" alt="use this qr code to share or join the chat" />
+      </div>
+      <div class="modal-action">
+        <button class="btn" on:click={closeModal}>Close</button>
+      </div>
+    </div>
+  </dialog>
 </main>
