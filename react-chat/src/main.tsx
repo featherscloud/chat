988436f93e8c1709a7ef013057db10d557a22b69
Karl Jensen <jensen@users.noreply.github.com>
2025-01-12 14:47:36 -0800

    fix: UI Updates, underline links and linebreak links (#6)
    Co-authored-by: David Luecke <daff@neyeon.com>

--- react-chat/src/main.tsx
+++ react-chat/src/main.tsx
@@ -5,9 +5,6 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <link href="https://cdn.jsdelivr.net/npm/daisyui@2.46.1/dist/full.css" rel="stylesheet" type="text/css" />
-    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2/dist/tailwind.min.css" rel="stylesheet" type="text/css" />
-    <link rel="stylesheet" href="https://dove.feathersjs.com/feathers-chat.css" />
     <App />
   </React.StrictMode>,
 )
