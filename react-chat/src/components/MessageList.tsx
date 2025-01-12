d0457ba0c41d3223caac132537c321a251bf58fb
thure <will@braneframe.com>
2025-01-11 22:11:43 -0800

    Remove reactions.

--- react-chat/src/components/MessageList.tsx
+++ react-chat/src/components/MessageList.tsx
@@ -1,5 +1,5 @@
-import {useCallback, useEffect, useRef} from "react"
-import { Message as MessageType, User } from "../utils"
+import {useCallback, useEffect, useRef, MouseEvent} from "react"
+import {Message as MessageType, User} from "../utils"
 import {useChat} from "./ChatContext.tsx";
 
 type MessageListProps = {
@@ -13,19 +13,43 @@ const formatDate = (timestamp: number) =>
     dateStyle: 'medium'
   }).format(new Date(timestamp))
 
-const Message= ({message, author }: {message: MessageType; author?: User;}) => {
+const Reaction = ({emoji, total, onClick}: { emoji: string, total: number, onClick: (event: MouseEvent) => void }) => {
+  return <button className='reaction' onClick={onClick} data-emoji={emoji}>
+    <span>{emoji}</span>
+    <span>{total}</span>
+  </button>;
+}
+
+const Message = ({message, author}: { message: MessageType; author?: User; }) => {
   const {user, handle} = useChat();
-  const reactToMessage = useCallback(() => {
-    handle?.change((chat)=>{
-      const writeableMessage = chat.messages.find(({id})=>id===message.id);
-      if(writeableMessage){
+  const handleAddReactClick = useCallback(() => {
+    handle?.change((chat) => {
+      const writeableMessage = chat.messages.find(({id}) => id === message.id);
+      if (writeableMessage) {
         writeableMessage.reactions ??= {};
         writeableMessage.reactions['✨'] ??= {};
         writeableMessage.reactions['✨'][user!.id] = true;
       }
     });
   }, [handle, message, user]);
-  const reactions = Object.entries(message.reactions??{}).reduce((acc: Record<string, number>, [emoji, peanutGallery])=>{
+  const handleRemoveReactClick = useCallback((event: MouseEvent) => {
+    const emoji = ((event.target as HTMLElement).closest('[data-emoji]') as HTMLButtonElement)?.getAttribute('data-emoji');
+    console.log('[handle remove]', emoji);
+    if (emoji && handle) {
+      handle?.change((chat) => {
+        const writeableMessage = chat.messages.find(({id}) => id === message.id);
+        if (writeableMessage) {
+          writeableMessage.reactions ??= {};
+          writeableMessage.reactions[emoji] ??= {};
+          delete writeableMessage.reactions[emoji][user!.id];
+          if(Object.keys(writeableMessage.reactions[emoji]).length < 1){
+            delete writeableMessage.reactions[emoji];
+          }
+        }
+      });
+    }
+  }, [handle, message, user]);
+  const reactions = Object.entries(message.reactions ?? {}).reduce((acc: Record<string, number>, [emoji, peanutGallery]) => {
     acc[emoji] = Object.keys(peanutGallery).length;
     return acc;
   }, {});
@@ -39,13 +63,14 @@ const Message= ({message, author }: {message: MessageType; author?: User;}) => {
       {author?.username}
       <time className="text-xs opacity-50">{formatDate(message.createdAt)}</time>
     </div>
-    <div className="chat-bubble">{message.text}</div>
-    <div className='reactions'>
-      {Object.entries(reactions).map(([emoji, total])=><span key={emoji}>
-          <span>{emoji}</span>
-          <span>{total}</span>
-        </span>)}
-      <button onClick={reactToMessage}>+</button>
+    <div className="chat-bubble relative">
+      {message.text}
+      <div className='absolute bottom-[-2rem] flex flex-wrap gap-1'>
+        {Object.entries(reactions).map(([emoji, total]) => <Reaction key={emoji} emoji={emoji} total={total}
+                                                                     onClick={handleRemoveReactClick}/>)}
+        {/*TODO(thure): Add emoji picker*/}
+        <button className='reaction' onClick={handleAddReactClick}>+</button>
+      </div>
     </div>
   </div>;
 }
@@ -64,6 +89,6 @@ export const MessageList = ({messages, users}: MessageListProps) => {
 
   return <div id="chat" className="h-full overflow-y-auto px-3">
     {messages.map(message => <Message key={message.id} message={message} author={usersById[message.userId]}/>)}
-    <div ref={messagesEndRef} />
+    <div ref={messagesEndRef}/>
   </div>
 }
