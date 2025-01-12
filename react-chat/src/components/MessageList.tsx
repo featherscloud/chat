85e82be3da7e753e22943d512eb1bce435358261
Karl Jensen <karljensen@gmail.com>
2025-01-12 14:43:59 -0800

    UI Updates, underline links and linebreak links

--- react-chat/src/components/MessageList.tsx
+++ react-chat/src/components/MessageList.tsx
@@ -15,16 +15,20 @@ const formatDate = (timestamp: number) =>
   }).format(new Date(timestamp))
 
 export const MessageList = ({ messages, users }: MessageListProps) => {
-  const messagesEndRef = useRef(null)
+  const messagesEndRef = useRef<HTMLDivElement>(null)
   const usersById = users.reduce((acc, user) => {
     acc[user.id] = user
     return acc
   }, {} as Record<string, User>)
 
+  useEffect(() => {
+    (messagesEndRef.current)?.scrollIntoView({ behavior: "auto" })
+  }, [])
+
   // Scroll to bottom when messages change
   useEffect(() => {
-    (messagesEndRef.current as any)?.scrollIntoView({ behavior: "smooth" })
-  })
+    (messagesEndRef.current)?.scrollIntoView({ behavior: "smooth" })
+  }, [messages])
 
   return <div id="chat" className="h-full overflow-y-auto px-3">
     {messages.map(message => <div className="chat chat-start py-2" key={message.id}>
@@ -33,11 +37,11 @@ export const MessageList = ({ messages, users }: MessageListProps) => {
           <img src={usersById[message.userId]?.avatar} />
         </div>
       </div>
-      <div className="chat-header pb-1">
-        {usersById[message.userId]?.username}
+      <div className="chat-header pb-1 space-x-2">
+        <span>{usersById[message.userId]?.username}</span>
         <time className="text-xs opacity-50">{formatDate(message.createdAt)}</time>
       </div>
-      <div className="chat-bubble"
+      <div className="chat-bubble break-words"
         dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(marked.parse(message.text))}} />
     </div>)}
     <div ref={messagesEndRef} />
